f5124817ddac64ef243dff8039cb820cb801ea6c
SirDraiger <lekht-k@yandex.ru>
2023-12-22 18:07:01 +0300

    error fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,18 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-const cardList = document.querySelector(".places__list");
+const cardsContainer = document.querySelector(".places__list");
 // @todo: Функция создания карточки
-function creatCard(name, link, delFunc) {
+function creatCard(cardData, delFunc) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const buttonDelete = cardElement.querySelector(".card__delete-button");
 
   buttonDelete.addEventListener("click", () => delFunc(cardElement));
 
-  // Заполняем ссылку на картику и текст заголовка карточки
-  cardElement.querySelector(".card__image").src = link;
-  cardElement.querySelector(".card__title").textContent = name;
+  // Заполняем ссылку на картику, альтернативный текст картинки и текст заголовка карточки
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = `Фотография: ${cardData.name}`;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
 
   return cardElement;
 }
@@ -21,6 +22,6 @@ function deleteCard(elem) {
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach((arr) => {
-  const newCard = creatCard(arr.name, arr.link, deleteCard);
-  cardList.append(newCard);
+  const newCard = creatCard(arr, deleteCard);
+  cardsContainer.append(newCard);
 });
