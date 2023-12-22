cce7a25df35b602bd668859b8db25cacd4900e0a
SirDraiger <lekht-k@yandex.ru>
2023-12-22 16:39:14 +0300

    card functionality

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/SirDraiger/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const cardList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function creatCard(name, link, delFunc) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const buttonDelete = cardElement.querySelector(".card__delete-button");
 
+  buttonDelete.addEventListener("click", () => delFunc(cardElement));
+
+  // Заполняем ссылку на картику и текст заголовка карточки
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__title").textContent = name;
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(elem) {
+  elem.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach((arr) => {
+  const newCard = creatCard(arr.name, arr.link, deleteCard);
+  cardList.append(newCard);
+});
